2624bb41d995b5635138a81cb61776961d96d3dd
Daan Kolthof <daan_kolthof@hotmail.com>
2020-04-11 20:46:55 +0100

    Implemented ALG message handling + cleanup

--- src/Node.py
+++ src/Node.py
@@ -12,10 +12,11 @@ def random_timeout():
 
 
 class NodeState(Enum):
-    passive = auto()
     candidate = auto()
     dummy = auto()
     leader = auto()
+    passive = auto()
+    waiting = auto()
 
 
 class Node:
@@ -29,6 +30,9 @@ class Node:
         self.candidate_successor = None
         self.candidate_predecessor = None
 
+        # Leader after election, will be set by the algorithm
+        self.leader = None
+
     def set_neighbour(self, neighbour_node):
         self.neighbour_node = neighbour_node
 
@@ -38,26 +42,51 @@ class Node:
         logging.info("Node %i has woken up", self.node_number)
 
         self.status = NodeState.candidate
-        # Construct an ALG message and send it to the node's neighbour
-        algMessage: Message = Message(MessageType.ALG, {})
-        self.neighbour_node.receive(self, algMessage)
 
-    def receive(self, from_node, message):
+        # Construct an ALG message and send it to the node's neighbour
+        alg_message: Message = Message(MessageType.ALG, {"init": self})
+        self.neighbour_node.receive(self, alg_message)
+
+    def receive(self, from_node, incoming_message):
         # Delay the receive a bit to allow other threads to do their thing as well (to simulate network traffic)
         random_timeout()
-        logging.info("Node %i  received '%s' from node %i", self.node_number, message, from_node.node_number)
+        logging.info("Node %i  received '%s' from node %i", self.node_number, incoming_message, from_node.node_number)
 
-        msgBody = message.msg_body
-        msgType: MessageType = message.msg_type
+        msg_body = incoming_message.msg_body
+        msg_type: MessageType = incoming_message.msg_type
 
-        if msgType == MessageType.ALG:
+        if msg_type == MessageType.ALG:
+            if NodeState.passive == self.status:
+                self.status = NodeState.dummy
+
+                alg_message: Message = incoming_message
+                self.neighbour_node.receive(self, alg_message)
+
+            elif NodeState.candidate == self.status:
+                self.candidate_predecessor = msg_body["init"]
+
+                if self.node_number > msg_body["init"].node_number:
+                    if not self.candidate_successor:
+                        self.status = NodeState.waiting
+
+                        avs_message: Message = Message(MessageType.AVS, {"j": self})
+                        msg_body["init"].receive(self, avs_message)
+                    else:
+                        avs_response_message: Message = Message(MessageType.AVS_RESP, {"k": self.candidate_predecessor})
+                        self.candidate_successor.receive(self, avs_response_message)
+
+                        self.status = NodeState.dummy
+
+                elif self.node_number == msg_body["init"].node_number:
+                    logging.info("Node %i has been made leader", self.node_number)
+                    self.status = NodeState.leader
+
+        elif msg_type == MessageType.AVS:
             exit()
-        elif msgType == MessageType.AVS:
-            exit()
-        elif msgType == MessageType.AVSRSP:
+        elif msg_type == MessageType.AVSRSP:
             exit()
         else:
-            raise RuntimeError("Unknown message type: " + msgType)
+            raise RuntimeError("Unknown message type: " + msg_type)
 
     def __str__(self):
         return "Node " + str(self.node_number) + " with neighbour: " + (
