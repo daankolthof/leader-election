7cec9736175ed065ef711725bd2ae6b9f3907330
Daan Kolthof <daan_kolthof@hotmail.com>
2020-04-16 20:21:25 +0100

    Clean up/refactor code as a different algorithm will be used (Bully algorithm)

--- src/Message.py
+++ src/Message.py
@@ -1,18 +1,24 @@
 from enum import Enum, auto
 
 
+class MessageType(Enum):
+    ELECTION = auto()
+    ALIVE = auto()
+    VICTORY = auto()
+
+    # Not part of the leader election algorithm, used by nodes to check each other.
+    PING = auto()
+    PONG = auto()
+
+    # Not part of the leader election algorithm, used for to instruct a node to simulate failure.
+    FAIL_NODE = auto()
+
+
 class Message:
 
-    def __init__(self, msg_type, msg_body):
+    def __init__(self, msg_type: MessageType, msg_body):
         self.msg_type = msg_type
         self.msg_body = msg_body
 
     def __str__(self):
         return "Message: {msg_type: " + str(self.msg_type) + ", msg_body: " + str(self.msg_body) + "}"
-
-
-class MessageType(Enum):
-    ALG = auto()
-    AVS = auto()
-    AVS_RESP = auto()
-    WAKEUP = auto()
--- src/Node.py
+++ src/Node.py
@@ -2,51 +2,79 @@ import time
 import logging
 import random
 import threading
-from enum import Enum, auto
 
 from src.Message import Message, MessageType
 
 
 def random_timeout():
-    #timeout = 1 + random.uniform(-0.1, 0.1)
     timeout = random.uniform(0, 0.01)
     time.sleep(timeout)
-
-
-class NodeState(Enum):
-    candidate = auto()
-    dummy = auto()
-    leader = auto()
-    passive = auto()
-    waiting = auto()
 
 
 class Node:
     leader_elected = None
     expected_leader = -1
 
-    def __init__(self, node_number):
+    def __init__(self, node_number, leader_node):
         self.node_number = node_number
-        self.neighbour_node = None
+        self.nodes_online_map = None
+        self.nodes_last_ping_map = None
 
         # Algorithm-specific variables
-        self.status = NodeState.passive
-        self.candidate_successor = None
-        self.candidate_predecessor = None
-
-        # Leader after election, will be set by the algorithm
-        self.leader = None
+        self.leader_node = leader_node
 
         self.lock = threading.Lock()
         self.message_queue = []
 
-    def set_neighbour(self, neighbour_node):
-        self.neighbour_node = neighbour_node
+        self.is_running = True
+
+        self.last_sent_ping_time = None
+
+    def set_all_nodes(self, all_nodes):
+        for node in all_nodes:
+            if self == node:
+                continue
+
+            self.nodes_online_map[node] = True
+            self.nodes_last_ping_map[node] = None
+
+    def send_pings(self):
+        current_time_seconds = int(time.time())
+
+        # If we haven't sent any pings yet or it has been 5 seconds since last ping sent out.
+        if not self.last_sent_ping_time or current_time_seconds > self.last_sent_ping_time+5:
+            ping_message: Message = Message(MessageType.PING, None)
+
+            for node, online_status in self.nodes_online_map:
+                if online_status:
+                    node.receive(self, ping_message)
+                    self.nodes_last_ping_map[node] = current_time_seconds
+
+            self.last_sent_ping_time = current_time_seconds
+
+    def check_for_pongs(self):
+        current_time_seconds = int(time.time())
+
+        # Check the ping map to see if any pings have not been answered within 2 seconds.
+        for node, last_ping_time in self.nodes_last_ping_map:
+            if last_ping_time and self.nodes_online_map[node] and last_ping_time + 2 < current_time_seconds:
+                # Failed to receive pong on time.
+                self.nodes_online_map[node] = False
+                if node == self.leader_node:
+                    # Leader node failed, trigger new leader election
+                    self.trigger_election()
+
+    def trigger_election(self):
+        logging.info("Node %i  has started new leader election", self.node_number)
+        # TODO: Implement this
 
     def run(self):
-        while not Node.leader_elected:
-            # Delay the wakeup a bit to allow other threads to do their thing as well (to simulate network traffic)
+
+        while self.is_running:
+            # Random delay to simulate read-world network traffic.
             random_timeout()
+
+            self.send_pings()
 
             from_node = None
             message = None
@@ -57,21 +85,7 @@ class Node:
                     from_node, message = self.message_queue.pop(0)
 
             if message:
-                if MessageType.WAKEUP == message.msg_type and NodeState.passive == self.status:
-                    self.wakeup()
-                else:
-                    self.process_message(from_node, message)
-
-    def wakeup(self):
-        logging.info("Node %i has woken up", self.node_number)
-
-        self.status = NodeState.candidate
-
-        # Construct an ALG message and send it to the node's neighbour
-        alg_message: Message = Message(MessageType.ALG, self)
-        self.neighbour_node.receive(self, alg_message)
-
-        Node.expected_leader = max(self.node_number, Node.expected_leader)
+                self.process_message(from_node, message)
 
     def receive(self, from_node, incoming_message):
         logging.info("Node %i  received '%s' from node %s", self.node_number, incoming_message, from_node)
@@ -81,76 +95,18 @@ class Node:
     def process_message(self, from_node, incoming_message):
         logging.info("Node %i (status:%s)  is processing '%s' from node %i", self.node_number, self.status, incoming_message, from_node.node_number)
 
-        msg_body = incoming_message.msg_body
         msg_type: MessageType = incoming_message.msg_type
+        msg_body = incoming_message.msg_body
 
-        if msg_type == MessageType.ALG:
-            if NodeState.passive == self.status:
-                self.status = NodeState.dummy
-
-                alg_message: Message = incoming_message
-                self.neighbour_node.receive(self, alg_message)
-
-            elif NodeState.candidate == self.status:
-                self.candidate_predecessor = msg_body
-
-                if self == msg_body:
-                    logging.info("Node %i has been made leader", self.node_number)
-                    self.status = NodeState.leader
-                    Node.leader_elected = self.node_number
-
-                else:
-                    if not self.candidate_successor:
-                        if self.node_number > msg_body.node_number:
-
-                            self.status = NodeState.waiting
-
-                            avs_message: Message = Message(MessageType.AVS, self)
-                            msg_body.receive(self, avs_message)
-
-                    else:
-                        avs_response_message: Message = Message(MessageType.AVS_RESP, self.candidate_predecessor)
-                        self.candidate_successor.receive(self, avs_response_message)
-
-                        self.status = NodeState.dummy
-
-        elif msg_type == MessageType.AVS:
-            if NodeState.candidate == self.status:
-                if not self.candidate_predecessor:
-                    self.candidate_successor = msg_body
-
-                else:
-                    avs_response_message: Message = Message(MessageType.AVS_RESP, self.candidate_predecessor)
-                    msg_body.receive(self, avs_response_message)
-
-                    self.status = NodeState.dummy
-
-            elif NodeState.waiting == self.status:
-                self.candidate_successor = msg_body
-
-        elif msg_type == MessageType.AVS_RESP:
-            if NodeState.waiting == self.status:
-                if self == msg_body:
-                    logging.info("Node %i has been made leader", self.node_number)
-                    self.status = NodeState.leader
-                    Node.leader_elected = self.node_number
-
-                else:
-                    self.candidate_predecessor = msg_body
-
-                    if not self.candidate_successor:
-                        if msg_body.node_number < self.node_number:
-                            self.status = NodeState.waiting
-
-                            avs_message: Message = Message(MessageType.AVS, self)
-                            msg_body.receive(self, avs_message)
-
-                    else:
-                        self.status = NodeState.dummy
-
-                        avs_response_message: Message = Message(MessageType.AVS_RESP, msg_body)
-                        self.candidate_successor.receive(self, avs_response_message)
-
+        if MessageType.PING == msg_type:
+            pong_message: Message = Message(MessageType.PONG, None)
+            from_node.receive(self, pong_message)
+        if MessageType.PONG == msg_type:
+            self.nodes_last_ping_map[from_node] = None
+            # self.nodes_online_map[from_node] = True # Not necessary for this simulation, failed nodes do not recover.
+        elif MessageType.FAIL_NODE == msg_type:
+            # 'Fail' the node, stopping its message processing loop.
+            self.is_running = False
         else:
             raise RuntimeError("Unknown message type: " + msg_type)
 
